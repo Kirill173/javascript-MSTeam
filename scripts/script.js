618d7710a21db5d0539a3eac721c63cf058cd785
Kirill173 <k.a.chugunov@yandex.ru>
2019-07-03 14:52:53 +0400

    Calculator

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,33 @@
-alert('Hello MediaSoft!');
+var firstNum = prompt('Введите первое значение?', '0');
+var operator = prompt('Операция?', '+');
+var secondNum = prompt('Введите второе значение?', '0');
+var result;
+
+function calculate(operator,num1,num2) {
+    
+    switch(operator){
+        
+        case '+':
+            return ((+num1) + (+num2));
+           
+            
+        case '*':
+            return ((+num1) * (+num2));
+          
+           
+        case '-':
+            return ((+num1) - (+num2));
+          
+            
+        case '/':
+            return ((+num1) / (+num2));
+                     
+        default:
+            alert("Неизвестная операция");
+            }
+    
+}
+
+
+result=calculate(operator, firstNum, secondNum);
+alert(result);
